679ca2b28827ffd4c0b029b2fa9df8ecde29f314
Lou CHARLET <lou.charlet@epitech.eu>
2026-06-02 16:15:45 +0200

    refactor: move lexical diversity into function

--- bookworm.py
+++ bookworm.py
@@ -7,41 +7,55 @@ from collections import Counter
 import urllib.request
 import re
 
-url = "https://www.gutenberg.org/files/11/11-0.txt"
 
-response = urllib.request.urlopen(url)
-text = response.read().decode("utf-8")
+def get_book(book_id):
 
-text = text.lower()
+    url = f"https://www.gutenberg.org/files/{book_id}/{book_id}-0.txt"
 
-#si pas lettre, remplacé par espace
-text = re.sub(r"[^a-z\s]", " ", text)
+    response = urllib.request.urlopen(url)
 
-words = text.split()
+    return response.read().decode("utf-8")
 
-unique_words = set(words)  #supprime doublons
 
-word_counts = Counter(words)
+def lexdiv(text):
 
-tok = len(words)  # nombre total de mots
-typ = len(unique_words)  # nb de mots différents
+    text = text.lower()
 
-hap = 0
-for word, count in word_counts.items():
-    if count == 1:
-        hap += 1  # nb de mots apparaissant 1 seule fois
+    #si pas lettre, remplacé par espace
+    text = re.sub(r"[^a-z\s]", " ", text)
 
-ttr = typ / tok  # nombre de mots différents / nb total de mots
-mwl = sum(len(word) for word in words) / tok  #longueur moyenne des mots
-mwf = tok / typ  # nombre total de mots /nombre de mots différents
+    words = text.split()
 
-result = {
-    "tok": tok,
-    "typ": typ,
-    "hap": hap,
-    "ttr": round(ttr, 4),
-    "mwl": round(mwl, 4),
-    "mwf": round(mwf, 4)
-}
+    unique_words = set(words)  #supprime doublons
+
+    word_counts = Counter(words)
+
+    tok = len(words)  # nombre total de mots
+    typ = len(unique_words)  # nb de mots différents
+
+    hap = 0
+    for word, count in word_counts.items():
+        if count == 1:
+            hap += 1  # nb de mots apparaissant 1 seule fois
+
+    ttr = typ / tok  # nombre de mots différents / nb total de mots
+    mwl = sum(len(word) for word in words) / tok  #longueur moyenne des mots
+    mwf = tok / typ  # nombre total de mots /nombre de mots différents
+
+    result = {
+        "tok": tok,
+        "typ": typ,
+        "hap": hap,
+        "ttr": round(ttr, 4),
+        "mwl": round(mwl, 4),
+        "mwf": round(mwf, 4)
+    }
+
+    return result
+
+
+text = get_book(11)
+
+result = lexdiv(text)
 
 print(result)
